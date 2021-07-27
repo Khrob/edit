30f10ace5763b1c4a785e8170894b09bb7bddfc2
Khrob Edmonds <khedmonds@gmail.com>
2021-07-27 11:07:58 +0800

    Less verbose, but more useful output

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 
 void silly_string_test()
 {
-	char string[128] = "Hola, mundo! And how are you this afternoon?";
+	const char string[128] = "Hola, mundo! And how are you this afternoon?";
 	int len = strlen(string);
 	int sleeptime = 1000000 / 55;
 
@@ -22,36 +22,36 @@ void silly_string_test()
 int test_write_file ()
 {
 	int res = -1;
-	int file_descriptor = open_file("test/writetest.file");
-	printf("writetest.file id is %d\n", file_descriptor);
-
-	if (file_descriptor == -1) { 
-		close_file(file_descriptor); 
-	}
-	else {
-		char *text = "there's only one Bengo Macarona.";
+	int file_descriptor = open_file((char *)"test/writetest.file");
+	if (file_descriptor != -1) { 
+		const char *text = "there's only one Bengo Macarona.";
 		res = write(file_descriptor, text, strlen(text));
+		close_file(file_descriptor);
 		if (res == -1) { printf("Error writing text\n"); }
-		else { printf ("res: %d\n", res); }
+		else { res = 0; }
 	}
-
-	close_file(file_descriptor);
-	return res;
+	return res == -1 ? 0 : 1;
 }
 
 int test_read_file ()
 {
-	void *data = read_file ("resources/test.file");
+	void *data = read_file ((char *)"resources/test.file");
 	if (data == NULL) { 
 		printf("FAIL: Couldn't read test.file.");
-		return -1;
-	} else { 
-		printf("%s\n", (char *)data);
 		return 0;
+	} else { 
+		// printf("%s\n", (char *)data);
+		return 1;
 	}
 }
 
 
+void red_text (const char *str) 
+{
+	printf ("\033[0;31m");
+	printf ("%s", str);
+	printf ("\033[0m"); 
+}
 
 int main () 
 {
@@ -60,8 +60,16 @@ int main ()
 
 	// silly_string_test();
 
-	test_write_file();
-	test_read_file();
+	if (test_write_file()) { passcount++; } else { red_text("Failed test_write_file\n"); failcount++; }
+	if (test_read_file())  { passcount++; } else { red_text("Failed test_read_file\n"); failcount++; }
+
+	printf ("Passed %d tests\n", passcount);
+
+	if (failcount > 0) {
+		char s[32];
+		sprintf(s, "Failed %d tests.\n", failcount);
+		red_text(s);
+	}
 
 	return 0;
 }
